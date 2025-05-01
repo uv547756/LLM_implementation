52da3ae5bdabb9987fe9a5439a1d1c7604604d6f
Komi <uv47756@gmail.com>
2025-05-02 01:14:32 +0530

    worked on encoding and decoding and a small txt for types of tokens

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import os
 import urllib.request
 import re
 
+class tokenizer:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
 if not os.path.exists("the-verdict.txt"):
   url = ("https://raw.githubusercontent.com/rasbt/"
            "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
@@ -23,8 +38,11 @@ vocab_size = len(all_words)
 #print(vocab_size) 1130
 
 vocab = {token:integer for integer,token in enumerate(all_words)}
-for i, item in enumerate(vocab.items()):
-    print(item)
-    if i >= 50:
-        break
 
+tokenize = tokenizer(vocab)
+text = """"It's the last he painted, you know," 
+           Mrs. Gisburn said with pardonable pride."""
+ids = tokenize.encode(text)
+print(ids)
+
+print(tokenize.decode(ids))
